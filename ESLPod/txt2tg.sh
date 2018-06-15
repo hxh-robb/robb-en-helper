4b177a3068187b997e5f30f8b1828708cddcf79a
hxh_robb <hxh.robb@gmail.com>
2018-06-15 09:56:51 +0800

    Add episodes

--- ESLPod/txt2tg.sh
+++ ESLPod/txt2tg.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 # Replace the html content from www.ieslpod.com to telegram formatting text
 
-echo TODO
+for txt in "$@"
+do
+  if [ -f "$txt" ]; then
+    tg=`echo "$txt"|sed -r 's/(.*)\.txt/\1.tg/g'`
+    if ! [ -e "$tg" ]; then
+      cat "$txt"|sed -r '1d; s/<br>//g; s/(.*)<strong>(.*)<\/strong>/\*\*\2\*\*/g; s/\*\s(.*)/__\1__/g; s/<\/.*>//g' > "$tg"
+    fi
+    xclip -sel clip "$tg"
+    less -SN "$tg"
+  else
+    echo "$txt - not exist!"
+  fi
+done
 #cat text/0001\ Introducing\ Yourself.txt|sed -r '1d; s/<br>//g; s/(.*)<strong>(.*)<\/strong>/\*\*\2\*\*/g; s/\*\s(.*)/__\1__/g; s/<\/.*>//g'|less -SN
